e4167fc380953063c21a91d8a1464d70e602243c
kingsley <chukwumakingsley1@gmail.com>
2024-03-10 21:45:21 +0200

    added :

--- routes/register.js
+++ routes/register.js
@@ -10,120 +10,138 @@ const axios = require("axios");
 // Set your SendGrid API key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-
-
 // Route to get all registrations
 router.get("/all_students", async (req, res) => {
-    try {
-        const registrations = await Register.find();
-        res.render("registration/index", {
-            registrations: registrations,
-            layout: false
-        })
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const registrations = await Register.find();
+    res.render("registration/index", {
+      registrations: registrations,
+      layout: false,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 const deleteOldRegistrations = async () => {
-    try {
-        const now = new Date();
-        const twoWeeksAgo = new Date(now.getTime() - 14 * 24 * 60 * 60 * 1000); // 14 days ago
+  try {
+    const now = new Date();
+    const twoWeeksAgo = new Date(now.getTime() - 14 * 24 * 60 * 60 * 1000); // 14 days ago
 
-        // Find and delete registrations created more than two weeks ago
-        await Register.deleteMany({
-            createdAt: { $lte: twoWeeksAgo }
-        });
+    // Find and delete registrations created more than two weeks ago
+    await Register.deleteMany({
+      createdAt: { $lte: twoWeeksAgo },
+    });
 
-        console.log('Old registrations (more than two weeks ago) deleted successfully.');
-    } catch (error) {
-        console.error('Error deleting old registrations:', error);
-    }
+    console.log(
+      "Old registrations (more than two weeks ago) deleted successfully."
+    );
+  } catch (error) {
+    console.error("Error deleting old registrations:", error);
+  }
 };
 
 // Run the function every 10 hours (in milliseconds)
 const intervalId = setInterval(deleteOldRegistrations, 10 * 60 * 60 * 1000); // 10 hours
 
-
-
-
 // Route to render the registration form
 router.get("/new", (req, res) => {
-    res.render("registration/register", { layout: false });
+  res.render("registration/register", { layout: false });
 });
-
-
 
 // Route to render the verification form
 router.get("/newverification", (req, res) => {
-    const message = "";
-    res.render("registration/verification", { message, layout: false });
+  const message = "";
+  res.render("registration/verification", { message, layout: false });
 });
 
 // Route to render register details if found in the database
 router.get("/verify", async (req, res) => {
-    const registrationCode = req.query.registrationcode;
+  const registrationCode = req.query.registrationcode;
 
-    try {
-        const userData = await Register.findOne({ registrationcode: registrationCode });
+  try {
+    const userData = await Register.findOne({
+      registrationcode: registrationCode,
+    });
 
-        if (userData) {
-            // Respond with JSON on success
-            res.render("registration/details-form", { userData, layout: false });
-        } else {
-            // Render the verification form with a message if no userData found
-            const message = "No user with the specified registration code found.";
-            res.render("registration/verification", { message, layout: false });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+    if (userData) {
+      // Respond with JSON on success
+      res.render("registration/details-form", { userData, layout: false });
+    } else {
+      // Render the verification form with a message if no userData found
+      const message = "No user with the specified registration code found.";
+      res.render("registration/verification", { message, layout: false });
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
-
-
 
 // Route to handle new student registration
 router.post("/student", async (req, res) => {
+  const newRegistration = new Register({
+    parent_firstname: req.body.parent_firstname,
+    parent_lastname: req.body.parent_lastname,
+    country: req.body.country,
+    state: req.body.state,
+    email: req.body.email,
+    cellnumber: req.body.cellnumber,
+    childs_fullname: req.body.childs_fullname,
+    child_age: req.body.child_age,
+    gender: req.body.gender,
+    grade: req.body.grade,
+  });
 
-    const newRegistration = new Register({
-        parent_firstname: req.body.parent_firstname,
-        parent_lastname: req.body.parent_lastname,
-        country: req.body.country,
-        state: req.body.state,
-        email: req.body.email,
-        cellnumber: req.body.cellnumber,
-        childs_fullname: req.body.childs_fullname,
-        child_age: req.body.child_age,
-        gender: req.body.gender,
-        grade: req.body.grade,
-    });
+  try {
+    await newRegistration.save();
 
-    try {
-        await newRegistration.save();
+    // Send email to the registered user with a link
+    await sendEmailConfirmation(
+      req.body.email,
+      req.body.parent_firstname,
+      req.body.parent_lastname,
+      newRegistration.registrationcode,
+      req.body.country,
+      req.body.state,
+      req.body.cellnumber,
+      req.body.childs_fullname,
+      req.body.child_age,
+      req.body.gender,
+      req.body.grade,
+      req.body.message
+    );
 
-        // Send email to the registered user with a link
-        await sendEmailConfirmation(req.body.email, req.body.parent_firstname, req.body.parent_lastname, newRegistration.registrationcode, req.body.country, req.body.state, req.body.cellnumber, req.body.childs_fullname, req.body.child_age, req.body.gender, req.body.grade, req.body.message);
-
-        res.status(200).json({ message: "Python script executed successfully" });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-        // You might want to handle the error differently based on your use case
-    }
+    res.status(200).json({ message: "Python script executed successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+    // You might want to handle the error differently based on your use case
+  }
 });
 
-
 // Function to send email confirmation using SendGrid
-async function sendEmailConfirmation(email, parent_firstname, parent_lastname, registrationCode, country, state, cellnumber, childs_fullname, child_age, gender, grade, message) {
-    const msg = {
-        to: email,
-        from: "register@youngafricanstech.org", // Replace with your SendGrid verified email
-        subject: "Registration Confirmation",
-        html: `
-      <h4>Your Registratio code is ${registrationCode}</h4> Dear <em>${parent_firstname} ${parent_lastname}</em>,<p>We extend our appreciation for your registration with Young Africans Tech.</p>
+async function sendEmailConfirmation(
+  email,
+  parent_firstname,
+  parent_lastname,
+  registrationCode,
+  country,
+  state,
+  cellnumber,
+  childs_fullname,
+  child_age,
+  gender,
+  grade,
+  message
+) {
+  const msg = {
+    to: email,
+    from: "register@youngafricanstech.org", // Replace with your SendGrid verified email
+    subject: "Registration Confirmation",
+    html: `
+      <h4>Your Registration code is: ${registrationCode}</h4> Dear <em>${parent_firstname} ${parent_lastname}</em>,<p>We extend our appreciation for your registration with Young Africans Tech.</p>
 
     <p><strong>Important Steps for Successful Program Admission</strong><br>To ensure your eligibility for our programs, please adhere to the following admission process steps:</p>
 
@@ -167,11 +185,9 @@ async function sendEmailConfirmation(email, parent_firstname, parent_lastname, r
     Young Africans Tech</h5>
 
     `,
-    };
+  };
 
-
-
-    const output = `
+  const output = `
                         <h3>Registration Details </h3>
                         <ul>
                             <li><h1>Parent Firstname: ${parent_firstname}</h1></li>
@@ -189,58 +205,60 @@ async function sendEmailConfirmation(email, parent_firstname, parent_lastname, r
                         <p>Request: ${message}</p>
                     `;
 
-    const my_msg = {
-        to: "register@youngafricanstech.org",
-        from: "register@youngafricanstech.org",
-        subject: "coding Application from " + childs_fullname,
-        text: output,
-        html: output,
-    };
+  const my_msg = {
+    to: "register@youngafricanstech.org",
+    from: "register@youngafricanstech.org",
+    subject: "coding Application from " + childs_fullname,
+    text: output,
+    html: output,
+  };
 
-    try {
-        await sgMail.send(msg);
-        await sgMail.send(my_msg);
+  try {
+    await sgMail.send(msg);
+    await sgMail.send(my_msg);
 
-
-        console.log("Email sent successfully");
-    } catch (error) {
-        console.error(error);
-        throw new Error("Failed to send email");
-    }
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to send email");
+  }
 }
-
 
 // Book interview
 router.post("/send", async (req, res) => {
-    const response_key = req.body["g-recaptcha-response"];
-    if (!req.body["g-recaptcha-response"]) {
-        return res.status(400).json({ error: "reCaptcha token is missing" });
-    }
-    var email_link = ""
-    try {
-        const googleVerifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${req.body["g-recaptcha-response"]}`;
-        const child_python = spawn("python3", ["routes/sendcalendar.py", req.body.email, req.body.interviewdate]);
+  const response_key = req.body["g-recaptcha-response"];
+  if (!req.body["g-recaptcha-response"]) {
+    return res.status(400).json({ error: "reCaptcha token is missing" });
+  }
+  var email_link = "";
+  try {
+    const googleVerifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${req.body["g-recaptcha-response"]}`;
+    const child_python = spawn("python3", [
+      "routes/sendcalendar.py",
+      req.body.email,
+      req.body.interviewdate,
+    ]);
 
-        child_python.stdout.on("data", (data) => {
-            email_link = data.toString();
-        });
+    child_python.stdout.on("data", (data) => {
+      email_link = data.toString();
+    });
 
-        child_python.stderr.on("data", (data) => {
-            console.log("stderr: " + data);
-        });
+    child_python.stderr.on("data", (data) => {
+      console.log("stderr: " + data);
+    });
 
-        child_python.on("close", async (code) => {
-            console.log("just closing now: " + code);
+    child_python.on("close", async (code) => {
+      console.log("just closing now: " + code);
 
-            try {
-                const response = await axios.get(googleVerifyUrl);
+      try {
+        const response = await axios.get(googleVerifyUrl);
 
-                const { success } = response.data;
+        const { success } = response.data;
 
-                if (success && req.body.email) {
-                    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        if (success && req.body.email) {
+          sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-                    const output = `
+          const output = `
                         <h3>Registration Details </h3>
                         <ul>
                             <li><h1>Parent Firstname: ${req.body.parent_firstname}</h1></li>
@@ -249,18 +267,15 @@ router.post("/send", async (req, res) => {
                          
                     `;
 
-                    const msg = {
-                        to: "register@youngafricanstech.org",
-                        from: "register@youngafricanstech.org",
-                        subject: "coding Application from " + req.body.childs_fullname,
-                        text: output,
-                        html: output,
-                    };
+          const msg = {
+            to: "register@youngafricanstech.org",
+            from: "register@youngafricanstech.org",
+            subject: "coding Application from " + req.body.childs_fullname,
+            text: output,
+            html: output,
+          };
 
-
-
-
-                    theMessage = `Next Steps for Your Yat Online Coding Class Application.
+          theMessage = `Next Steps for Your Yat Online Coding Class Application.
 
                         <h2>Dear ${req.body.parent_firstname} ${req.body.parent_lastname}</h2>
 
@@ -285,34 +300,34 @@ router.post("/send", async (req, res) => {
                         
                     `;
 
-                    const secondMessage = {
-                        to: req.body.email,
-                        from: "register@youngafricanstech.org",
-                        subject: "YATA Interview for " + req.body.childs_fullname,
-                        text: theMessage,
-                        html: theMessage,
-                    };
+          const secondMessage = {
+            to: req.body.email,
+            from: "register@youngafricanstech.org",
+            subject: "YATA Interview for " + req.body.childs_fullname,
+            text: theMessage,
+            html: theMessage,
+          };
 
-                    sgMail.send(msg);
-                    sgMail.send(secondMessage);
+          sgMail.send(msg);
+          sgMail.send(secondMessage);
 
-                    // Send the appropriate response after sending emails
-                    res.status(200).json({ message: "Python script executed successfully" });
-
-                } else {
-                    console.log("check body, name, or success");
-                    res.status(400).json({ error: "Check body, name, or success" });
-                }
-            } catch (error) {
-                console.log(error);
-                res.status(500).json({ error: "Error with reCaptcha verification" });
-            }
-        });
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({ error: "An error occurred" });
-    }
+          // Send the appropriate response after sending emails
+          res
+            .status(200)
+            .json({ message: "Python script executed successfully" });
+        } else {
+          console.log("check body, name, or success");
+          res.status(400).json({ error: "Check body, name, or success" });
+        }
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Error with reCaptcha verification" });
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
-
 
 module.exports = router;
